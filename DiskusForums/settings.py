7dff61450ecdd09af81320b48d719b94aaa928ef
Chirag Jain <chirag@Chirags-MacBook-Pro.local>
2015-10-30 14:56:20 +0530

    Debug off

--- DiskusForums/settings.py
+++ DiskusForums/settings.py
@@ -27,7 +27,7 @@ TEMPLATE_PATH = os.path.join(PROJECT_PATH, 'templates')
 SECRET_KEY = 'vyw*6)*+rzk-jhotux#t@gr(bb!&p8!w&c4cg_8)r1(&fgnmmj'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
